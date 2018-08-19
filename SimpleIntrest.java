96c35f3d5c9729540a95a08dd18a3ffa3deef9b4
chandravavilala <chandra.vavilala@gmail.com>
2018-08-18 20:09:29 -0500

    SimpleIntrest project and updated text file with basics

---
+++ SimpleIntrest.java
@@ -0,0 +1,14 @@
+class SimpleIntrest
+{
+    public static void main(String args[]){
+        double si;
+        int p, t, r;
+
+        p=100000;
+        t=2;
+        r=2;
+
+        si=(p*t*r)/100;
+        System.out.println("Simple Intrest: "+si);
+    }
+}
