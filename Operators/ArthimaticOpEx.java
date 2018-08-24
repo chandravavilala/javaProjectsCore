d89bf2bb1a104606f1de08ec228d2f2bca7bda0b
chandravavilala <chandra.vavilala@gmail.com>
2018-08-23 20:15:48 -0500

    Operators project folder added

---
+++ Operators/ArthimaticOpEx.java
@@ -0,0 +1,41 @@
+class ArthimaticOpEx
+{
+    public static void main(String args[])
+    {
+        int num1 = 20;
+        int num2 = 5;
+        int addition;
+        int subtraction;
+        int multiplication;
+        int division;
+        int modulus;
+        int increment;
+        int decrement;
+
+        System.out.println("Arthimatic operatios example with num1=20 and num2=5");
+
+        addition=num1+num2;
+        System.out.println("+ :"+addition);
+
+        subtraction=num1-num2;
+        System.out.println("- :"+subtraction);
+
+        multiplication=num1*num2;
+        System.out.println("* :"+ multiplication);
+
+        division=num1/num2;
+        System.out.println("/ :"+division);
+
+        modulus=num1%num2;
+        System.out.println("% :"+modulus);
+
+        increment=++num1;
+        System.out.println("++ :"+increment);
+
+        decrement=--num2;
+        System.out.println("-- :"+decrement);
+
+/* Arthimatic operators usage */
+
+    }
+}
