7bb15fcc7bb40ac737ea07edcd0de0e415a2a02d
chandravavilala <chandra.vavilala@gmail.com>
2018-08-23 20:34:20 -0500

    Relational operators project added

---
+++ Operators/RelationshipOpEx.java
@@ -0,0 +1,30 @@
+class RelationshipOpEx
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+
+        System.out.println("Relational operatgrs true or false");
+        System.out.println("Relational operators ex with a=10, b=20");
+
+
+        System.out.print("10>20 :");
+        System.out.println(a>b);
+        System.out.print("10<20 :");
+        System.out.println(a<b);
+
+        System.out.print("10==20 :");
+        System.out.println(a==b);
+        System.out.print("10!=20 :");
+        System.out.println(a!=b);
+
+        System.out.print("10>=20 :");
+        System.out.println(a>=b);
+        System.out.print("10<=20 :");
+        System.out.println(a<=b);
+
+        
+
+    }
+}
